e4540855c3a3a3b22de091b691653b26f6df7a3a
guen <guen5997@naver.com>
2023-03-03 17:04:07 +0900

    Fragment 추가 및 화면 연결

---
+++ app/src/main/java/org/techtown/shoppi_android/HomeFragment.kt
@@ -0,0 +1,32 @@
+package org.techtown.shoppi_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false) // hostActivity가 모두 구성된 이후에 inflate 되어야함. false로 생성시점 늦춤
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.btn)
+
+        button.setOnClickListener {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.container_main, ProductDetailFragment())
+            transaction.commit()
+        }
+    }
+}
