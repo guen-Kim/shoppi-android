8ad4e5a86f43aaf51b002ba176891aecd4a1bdb1
guen <guen5997@naver.com>
2023-03-03 20:54:37 +0900

    Bottom Navigation 추가

--- app/src/main/java/org/techtown/shoppi_android/HomeFragment.kt
+++ app/src/main/java/org/techtown/shoppi_android/HomeFragment.kt
@@ -1,26 +1,33 @@
 package org.techtown.shoppi_android
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false) // hostActivity가 모두 구성된 이후에 inflate 되어야함. false로 생성시점 늦춤
+        return inflater.inflate(
+            R.layout.fragment_home,
+            container,
+            false
+        ) // hostActivity가 모두 구성된 이후에 inflate 되어야함. false로 생성시점 늦춤
 
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val button = view.findViewById<Button>(R.id.btn)
 
         button.setOnClickListener {
@@ -29,4 +36,5 @@ class HomeFragment: Fragment() {
             transaction.commit()
         }
     }
+
 }
--- app/src/main/java/org/techtown/shoppi_android/MainActivity.kt
+++ app/src/main/java/org/techtown/shoppi_android/MainActivity.kt
@@ -1,15 +1,22 @@
 package org.techtown.shoppi_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // app themes 색상 참조하지 않음.
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
 
 
 
     }
+
 }
