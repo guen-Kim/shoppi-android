3074e0baec70a2abe324f5252c6b7fe0323e055f
guen <guen5997@naver.com>
2023-03-04 15:50:20 +0900

    navigation component 와 Bottom navigation 연동 - 화면전환 구현

--- app/src/main/java/org/techtown/shoppi_android/MainActivity.kt
+++ app/src/main/java/org/techtown/shoppi_android/MainActivity.kt
@@ -1,9 +1,10 @@
 package org.techtown.shoppi_android
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         bottomNavigationView.itemIconTintList = null
 
+        //
+         val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let{
+            bottomNavigationView.setupWithNavController(it)
+        }
 
 
     }
