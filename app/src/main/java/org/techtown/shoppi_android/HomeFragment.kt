d81f50927fdd05ad254768b804cfc3183baaba65
guen <guen5997@naver.com>
2023-03-08 15:24:35 +0900

    HomeFragment 작업 - banner 위치 조정 및 Indicator 구현

--- app/src/main/java/org/techtown/shoppi_android/HomeFragment.kt
+++ app/src/main/java/org/techtown/shoppi_android/HomeFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 
 import org.json.JSONObject
@@ -60,6 +61,20 @@ class HomeFragment : Fragment() {
                 submitList(homeData.topBanners)
 
             }
+
+
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+            // 다음 페이지 위치
+            viewpager.offscreenPageLimit = 3
+            viewpager.setPageTransformer{page, position ->
+                page.translationX = position * -offset
+            }
+
+            TabLayoutMediator(viewpagerIndicator, viewpager
+            ) { tab, position -> }.attach()
 
 
         }
